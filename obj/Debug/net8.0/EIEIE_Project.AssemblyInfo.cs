d477a313c5e76beca28207ee377eced824b2a5b2
lolaop5047 <kthh9828@naver.com>
2025-02-10 09:30:40 +0900

    작업파윌 위치 수정--

--- obj/Debug/net8.0/EIEIE_Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/EIEIE_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EIEIE_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b0fa3d71e447542d02a7e45f2098a8040631da")]
 [assembly: System.Reflection.AssemblyProductAttribute("EIEIE_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EIEIE_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
