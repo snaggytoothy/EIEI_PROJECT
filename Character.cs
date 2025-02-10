62f75d3b28a2a1e56d5acd60161a1d6554be6155
lolaop5047 <kthh9828@naver.com>
2025-02-10 16:44:02 +0900

    [Character] 몬스터 추가

--- Character.cs
+++ Character.cs
@@ -77,7 +77,7 @@ public class Monster : Character // Monster 캐릭터 관리
             this.MonsterType = 2;
             this.MonsterID = id;
             this.Level = 6;
-            this.Name = "홉 고블린[브론즈]"; // EasyScreen 
+            this.Name = "홉 고블린[레어 브론즈]"; // EasyScreen 
             this.Atk = 6;
             this.Def = 5;
             this.MaxHP = 45;
@@ -92,7 +92,7 @@ public class Monster : Character // Monster 캐릭터 관리
             this.Name = "스켈레톤[스톤]"; // NormalScreen
             this.Atk = 2;
             this.Def = 1;
-            this.MaxHP = 20;
+            this.MaxHP = 15;
             this.NowHP = 20;
             Console.WriteLine("설명: 아무 장비도 장착 안 되어있는 그냥 스켈레톤이다 하지만 무리지어 다니기 때문에 다구리에 장사없으니 조심하자.");
         }
@@ -100,11 +100,11 @@ public class Monster : Character // Monster 캐릭터 관리
         {
             this.MonsterType = 1;
             this.MonsterID = id;
-            this.Level = 3;
+            this.Level = 4;
             this.Name = "궁수 스켈레톤[스톤]"; // NormalScreen
-            this.Atk = 4;
+            this.Atk = 5;
             this.Def = 2;
-            this.MaxHP = 20;
+            this.MaxHP = 15;
             this.NowHP = 20;
             Console.WriteLine("설명: 활, 가죽 누더기를 장착하고있다, 죽기전에는 궁수였던거 같다.");
 
@@ -117,72 +117,73 @@ public class Monster : Character // Monster 캐릭터 관리
             this.Level = 3;
             this.Name = "전사 스켈레톤[스톤]"; // NormalScreen
             this.Atk = 4;
-            this.Def = 2;
-            this.MaxHP = 20;
+            this.Def = 3;
+            this.MaxHP = 16;
             this.NowHP = 20;
-            Console.WriteLine("설명: ");
+            Console.WriteLine("설명: 들고있는 근접 무기가 다양하다, 대부분 녹슨 사슬갑옷을 장착, 죽기전에 전사였던거 같다.");
 
         }
-        else if (id == ) // 수진님 몬스터
+        else if (id == 8) // 수진님 몬스터
         {
-            this.MonsterType = 1;
+            this.MonsterType = 2;
             this.MonsterID = id;
-            this.Level = 3;
-            this.Name = "고스트[브론즈]"; //  
-            this.Atk = 3;
-            this.Def = 5;
-            this.MaxHP = 50;
-            this.NowHP = 50;
+            this.Level = 7;
+            this.Name = "War 고스트[레어 아이언]"; // NormalScreen
+            this.Atk = 7;
+            this.Def = 6;
+            this.MaxHP = 70;
+            this.NowHP = 70;
+            Console.WriteLine("설명: 검은 판급갑옷, 검은 대검 or 검은 방패 한손무기 장착, 발생 원인은 파악을 못 하였으나 전쟁터에서 죽은 사람의 원혼이 뭉쳐서 탄생한다는 소문이 있다, ");
         }
 
-        else if (id == )
-        {
-            this.MonsterType = 1;
-            this.MonsterID = id;
-            this.Level = 8;
-            this.Name = "물의 요정[]";
-            this.Atk = 12;
-            this.Def = 5;
-            this.MaxHP = 80;
-            this.NowHP = 80;
-        }
-        else if (id == )
-        {
-            this.MonsterType = 3;
-            this.MonsterID = id;
-            this.Level = 5;
-            this.Name = "킹 고블린 Boss[실버]";
-            this.Atk = 10;
-            this.Def = 10;
-            this.MaxHP = 200;
-            this.NowHP = 200;
-        }
-        else if (id == )
-        {
-            this.MonsterType = 3;
-            this.MonsterID = id;
-            this.Level = 10;
-            this.Name = "정령왕 픽시 Boss[]";
-            this.Atk = 30;
-            this.Def = 5;
-            this.MaxHP = 450;
-            this.NowHP = 450;
-        }
-        else if (id == )
-        {
-            this.MonsterType = 3;
-            this.MonsterID = id;
-            this.Level = 15;
-            this.Name = "언데드 군단장 Boss[]";
-            this.Atk = 30;
-            this.Def = 5;
-            this.MaxHP = 600;
-            this.NowHP = 600;
-        }
-        else
-        {
-            //예외처리
-        }
+        //else if (id == )
+        //{
+        //    this.MonsterType = 1;
+        //    this.MonsterID = id;
+        //    this.Level = 8;
+        //    this.Name = "물의 요정[]";
+        //    this.Atk = 12;
+        //    this.Def = 5;
+        //    this.MaxHP = 80;
+        //    this.NowHP = 80;
+        //}
+        //else if (id == )
+        //{
+        //    this.MonsterType = 3;
+        //    this.MonsterID = id;
+        //    this.Level = 5;
+        //    this.Name = "킹 고블린 Boss[실버]";
+        //    this.Atk = 10;
+        //    this.Def = 10;
+        //    this.MaxHP = 200;
+        //    this.NowHP = 200;
+        //}
+        //else if (id == )
+        //{
+        //    this.MonsterType = 3;
+        //    this.MonsterID = id;
+        //    this.Level = 10;
+        //    this.Name = "정령왕 픽시 Boss[]";
+        //    this.Atk = 30;
+        //    this.Def = 5;
+        //    this.MaxHP = 450;
+        //    this.NowHP = 450;
+        //}
+        //else if (id == )
+        //{
+        //    this.MonsterType = 3;
+        //    this.MonsterID = id;
+        //    this.Level = 15;
+        //    this.Name = "언데드 군단장 Boss[]";
+        //    this.Atk = 30;
+        //    this.Def = 5;
+        //    this.MaxHP = 600;
+        //    this.NowHP = 600;
+        //}
+        //else
+        //{
+        //    //예외처리
+        //}
 
     }
 }
