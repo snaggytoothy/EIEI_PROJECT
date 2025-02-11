e7a160fffc2deca3131041b6e0b71b424ebb2d47
lolaop5047 <kthh9828@naver.com>
2025-02-11 19:16:03 +0900

    [Character] Details 변수 추가

--- Character.cs
+++ Character.cs
@@ -17,6 +17,7 @@ public class Character
         }
     }
     public string Name { get; set; }
+    public string Details { get; set; }
     protected float atk;
     public virtual float Atk
     {
@@ -287,7 +288,7 @@ public class Monster : Character // Monster 캐릭터 관리
             this.Def = 1;
             this.MaxHP = 30;
             this.NowHP = 30;
-            Console.WriteLine("설명: 동그란 흰색 슬라임이다.");
+            this.Details = "설명: 동그란 흰색 슬라임이다.";
         }
         else if (id == 2)
         {
@@ -299,7 +300,7 @@ public class Monster : Character // Monster 캐릭터 관리
             this.Def = 3;
             this.MaxHP = 28;
             this.NowHP = 28;
-            Console.WriteLine("설명: 맨손상태, 가죽으로 쪼가리 몸에 두르고있다.");
+            this.Details = "설명: 맨손상태, 가죽으로 쪼가리 몸에 두르고있다.";
         }
         else if (id == 3)
         {
@@ -311,7 +312,7 @@ public class Monster : Character // Monster 캐릭터 관리
             this.Def = 4;
             this.MaxHP = 33;
             this.NowHP = 33;
-            Console.WriteLine("설명: 칼날 다 빠지고 녹슨 롱소드를 손에 쥐고 있다, 흉부에 가죽 갑옷을 입고있다");
+            this.Details = "설명: 칼날 다 빠지고 녹슨 롱소드를 손에 쥐고 있다, 흉부에 가죽 갑옷을 입고있다";
         }
         else if (id == 4)
         {
@@ -323,7 +324,7 @@ public class Monster : Character // Monster 캐릭터 관리
             this.Def = 5;
             this.MaxHP = 54;
             this.NowHP = 54;
-            Console.WriteLine("설명: 키가 사람 평균 정도다, 제대로 된 가죽 갑옷을 입고 있다, 무기는 조잡하게 만든 몽둥이");
+            this.Details = "설명: 키가 사람 평균 정도다, 제대로 된 가죽 갑옷을 입고 있다, 무기는 조잡하게 만든 몽둥이";
         }
         else if (id == 5)
         {
@@ -335,7 +336,7 @@ public class Monster : Character // Monster 캐릭터 관리
             this.Def = 1;
             this.MaxHP = 15;
             this.NowHP = 15;
-            Console.WriteLine("설명: 아무 장비도 장착 안 되어있는 그냥 스켈레톤이다 하지만 무리지어 다니기 때문에 다구리에 장사없으니 조심하자.");
+            this.Details = "설명: 아무 장비도 장착 안 되어있는 그냥 스켈레톤이다 하지만 무리지어 다니기 때문에 다구리에 장사없으니 조심하자.";
         }
         else if (id == 6)
         {
@@ -347,8 +348,7 @@ public class Monster : Character // Monster 캐릭터 관리
             this.Def = 2;
             this.MaxHP = 20;
             this.NowHP = 20;
-
-            Console.WriteLine("설명: 활, 가죽 누더기를 장착하고있다, 죽기전에는 궁수였던거 같다.");
+            this.Details = "설명: 활, 가죽 누더기를 장착하고있다, 죽기전에는 궁수였던거 같다.";
 
         }
         else if (id == 7)
@@ -361,7 +361,7 @@ public class Monster : Character // Monster 캐릭터 관리
             this.Def = 3;
             this.MaxHP = 23;
             this.NowHP = 23;
-            Console.WriteLine("설명: 들고있는 근접 무기가 다양하다, 대부분 녹슨 사슬갑옷을 장착, 죽기전에 전사였던거 같다.");
+            this.Details = "설명: 들고있는 근접 무기가 다양하다, 대부분 녹슨 사슬갑옷을 장착, 죽기전에 전사였던거 같다.";
 
         }
 
@@ -373,9 +373,9 @@ public class Monster : Character // Monster 캐릭터 관리
             this.Name = "스피어 군단장 언데드[등급: 브론즈]"; // NormalScreen
             this.Atk = 4;
             this.Def = 3;
-            this.MaxHP = 23;
-            this.NowHP = 23;
-            Console.WriteLine("설명: 글레이즈 폴암, 부서진 파급 갑옷 장착, 꽤나 좋은 품질을 장비를 하고 있다 ");
+            this.MaxHP = 40;
+            this.NowHP = 40;
+            this.Details = "설명: 글레이즈 폴암, 부서진 파급 갑옷 장착, 꽤나 좋은 품질을 장비를 하고 있다 ";
 
         }
         else if (id == 9) //수정중
@@ -384,11 +384,11 @@ public class Monster : Character // Monster 캐릭터 관리
             this.MonsterID = id;
             this.Level = 8;
             this.Name = "츠바이헨더 군단장 언데드[등급: 레어 브론즈]"; // NormalScreen
-            this.Atk = 4;
-            this.Def = 3;
-            this.MaxHP = 23;
-            this.NowHP = 23;
-            Console.WriteLine("설명: 은빛 츠바이헨더, 사슬+판급 갑옷 장착, 공략하면 낮은 확률로 은빛 츠바이헨더가 드랍된다는 소문이 있다.  살아생전 꽤나 실력이 있는 군단장이었던 거 같다 하지만 죽었죠?... ");
+            this.Atk = 7;
+            this.Def = 4;
+            this.MaxHP = 40;
+            this.NowHP = 40;
+            this.Details = "설명: 은빛 츠바이헨더, 사슬+판급 갑옷 장착, 살아생전 꽤나 실력이 있는 군단장이었던 거 같다 하지만 죽었죠?... "; // 공략하면 낮은 확률로 은빛 츠바이헨더가 드랍된다는 소문이 있다.  
             // 10 분의 1 확률로 은빛 츠바이헨더 드랍 기능 구현
         }
         else if (id == 10) // 수진님 몬스터
@@ -401,7 +401,7 @@ public class Monster : Character // Monster 캐릭터 관리
             this.Def = 7;
             this.MaxHP = 63;
             this.NowHP = 63;
-            Console.WriteLine("설명: 검은 판급갑옷, 검은 대검 or 검은 방패 한손무기 장착, 발생 원인은 파악을 못 하였으나 전쟁터에서 죽은 사람의 원혼이 뭉쳐서 탄생한다는 소문이 있다, ");
+            this.Details = "설명: 검은 판급갑옷, 검은 대검 or 검은 방패 한손무기 장착, 발생 원인은 파악을 못 하였으나 전쟁터에서 죽은 사람의 원혼이 뭉쳐서 탄생한다는 소문이 있다, ";
         }
         else if (id == 11)
         {
@@ -413,8 +413,8 @@ public class Monster : Character // Monster 캐릭터 관리
             this.Def = 5;
             this.MaxHP = 50;
             this.NowHP = 50;
-            Console.WriteLine("설명: 둔기 or 도끼를 들고 있다, 갑옷은 인간의 뼈로 만든 흉갑이며 기본 2미터 크기이다. ");
-            //Console.WriteLine("비스트맨의 종은 다양하다 하지만 이 던전에는 돌산양 비스트맨이 무리지어 살고있다. 얼굴은 돌산양 상반신 인간 하반신 말 이다. 박치기를 맞으면 상대는 대부분 즉사 한다고 하니 조심하자.");  던전 입장시 설명문으로 사용 예정 -> Dungeon 이동
+            this.Details = "설명: 둔기 or 도끼, 갑옷은 인간의 뼈로 만든 흉갑 장착, 항상 전투에 목말라있다. ";
+            //Console.WriteLine("비스트맨의 종은 다양하다 하지만 이 던전에는 돌산양 비스트맨이 무리지어 살고있다. 기본 2미터 크기이며 얼굴은 돌산양 상반신 인간 하반신 말 이다. 박치기를 맞으면 상대는 대부분 즉사 한다고 하니 조심하자.");  던전 입장시 설명문으로 사용 예정 -> Dungeon 이동
         }   //Console.WriteLine("선호하는 식량은 인간이며 둔기로 전투불능으로 만들어 신선하게 먹는 걸 좋아한다.")
         else if (id == 12)
         {
@@ -426,7 +426,7 @@ public class Monster : Character // Monster 캐릭터 관리
             this.Def = 7;
             this.MaxHP = 65;
             this.NowHP = 65;
-            Console.WriteLine("설명: 쌍수 시미터, 체인+판급 합성 갑옷을 장착, 인간 전사를 사냥해서 전리품을 장착한다, 큰 체구에 비해 상당히 민첩하며 괜찮은 장비를 보유하고있어서 공략하기 힘들다. ");
+            this.Details = "설명: 배틀액스, 판급갑옷 장착, 인간을 죽여서 장비를 장착하기도 한다, 전투에 노련한 자들이다 발차기를 조심하자.  ";
         }
         else if (id == 13)
         {
@@ -438,8 +438,8 @@ public class Monster : Character // Monster 캐릭터 관리
             this.Def = 5;
             this.MaxHP = 50;
             this.NowHP = 50;
-            Console.WriteLine("설명: 쌍수 시미터, 체인+판급 합성 갑옷을 장착, 인간 전사를 사냥해서 전리품을 장착한다, 큰 체구에 비해 상당히 민첩하며 괜찮은 장비를 보유하고있어서 공략하기 힘들다. ");
-            Console.WriteLine("설명: 오랫동안 죽지않고 사냥으로 경험을 쌓은 헌터 비스트맨은 골드 등급 까지도 갈수 있다. ");
+            this.Details = "설명: 쌍수 시미터, 경번갑 갑옷 장착, 인간 전사를 사냥해서 전리품을 장착한다, 큰 체구에 비해 상당히 민첩하며 괜찮은 장비를 보유하고있어서 공략하기 힘들다. ";
+            //설명: 오랫동안 죽지않고 사냥으로 경험을 쌓은 헌터 비스트맨은 골드 등급 까지도 갈수 있다. ";
         }
 
 
