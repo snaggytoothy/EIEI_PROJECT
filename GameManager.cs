7027fd5635ac6f0e8bbed3fa9565e87f80841cf2
lolaop5047 <kthh9828@naver.com>
2025-02-11 14:57:34 +0900

    [GameManager] 등급추가

--- GameManager.cs
+++ GameManager.cs
@@ -7,29 +7,29 @@ public class GameManager
         InitItem();
     }
 
-    public List<Item> itemList = new List<Item>()
+    public List<Item> itemList = new List<Item>() // 등급 : 일반 -> 명작 -> 레어 -> 전설 (등급 마다 성급 추가 최대 ★x3개)
     {
-         new Weapon{Name = "투박한 롱소드", ItemID = 1, Price = 500, ItemType = 0, Inform = "매우 흔하디흔한 양산형, 롱소드이다.",
+         new Weapon{Name = "투박한 롱소드[일반]", ItemID = 1, Price = 500, ItemType = 0, Inform = "매우 흔하디흔한 양산형, 롱소드이다.",
         IsEquiped = false, ShopFlag = true, MonsterFlag = false, Atk = 2f},
 
-        new Weapon{Name = "바스타드 소드", ItemID = 2, Price = 1150, ItemType = 0, Inform = "대검 반 한 손 검, 롱소드보다 바디의 길이가 더 길며 파괴력이 높고 공방 밸런스가 잘 갖춰졌다.",
+        new Weapon{Name = "바스타드 소드[일반★]", ItemID = 2, Price = 1150, ItemType = 0, Inform = "대검 반 한 손 검, 롱소드보다 바디의 길이가 더 길며 파괴력이 높고 공방 밸런스가 잘 갖춰졌다.",
         IsEquiped = false, ShopFlag = true, MonsterFlag = true, Atk = 4f},
 
-        new Weapon{Name = "한손검 글라디우스", ItemID = 3, Price = 600, ItemType = 0, Inform = "폭 넓은 양날 장검 무거워서 신속하게 사용 힘듬, 방패랑 사용하면 안정적이다.",
+        new Weapon{Name = "한손검 글라디우스[일반]", ItemID = 3, Price = 600, ItemType = 0, Inform = "폭 넓은 양날 장검 무거워서 신속하게 사용 힘듬, 방패랑 사용하면 안정적이다.",
         IsEquiped = false, ShopFlag = true, MonsterFlag = true, Atk = 3f,},
 
-        new Armor{Name = "레더 아머", ItemID = 4, Price = 500, ItemType = 1, Inform = "가죽을 무질과 간단한 기름칠로 경화처리를 거친, 흔한 가죽 갑옷.",
+        new Armor{Name = "레더 아머[일반]", ItemID = 4, Price = 500, ItemType = 1, Inform = "가죽을 무질과 간단한 기름칠로 경화처리를 거친, 흔한 가죽 갑옷.",
         IsEquiped = false, ShopFlag = true, MonsterFlag = false, Def = 1f},
 
-        new Armor{Name = "보일드 레더 아머", ItemID = 5, Price = 1000, ItemType = 1, Inform = "끓여진 파라핀+기름에 뺏다 넣었다 반복해서 만든 가죽 갑옷 끝판왕, 강도는 손으로 안 구부러질 정도다.",
+        new Armor{Name = "보일드 레더 아머[일반]", ItemID = 5, Price = 1000, ItemType = 1, Inform = "끓여진 파라핀+기름에 뺏다 넣었다 반복해서 만든 가죽 갑옷 끝판왕, 강도는 손으로 안 구부러질 정도다.",
         IsEquiped = false, ShopFlag = true, MonsterFlag = true, Def = 3f},
 
-        new Armor{Name = "고블린 찰갑", ItemID = 6, Price = 1600, ItemType = 1, Inform = "고블린 뼈로 만든 미늘 조각에 끈으로 연결하여 만든 갑옷, 꽤나 튼튼하고 유지 보수가 쉽다.",
+        new Armor{Name = "고블린 찰갑[일반★]", ItemID = 6, Price = 1600, ItemType = 1, Inform = "고블린 뼈로 만든 미늘 조각에 끈으로 연결하여 만든 갑옷, 꽤나 튼튼하고 유지 보수가 쉽다.",
         IsEquiped = false, ShopFlag = true, MonsterFlag = true, Def = 5f},
 
         new Consumable{Name = "회복 물약", ItemID = 7, Price = 50, ItemType = 2, Inform = "체력을 50 회복시켜 줍니다.", ShopFlag = true, MonsterFlag = false, BuffAmount = 50},
 
-        new Weapon{Name = "링 오브 고블린 로드 ", ItemID = 100, Price = 10000, ItemType = 1, Inform = "고블린 로드가 장착했던 반지, ", //작업중
+        new Weapon{Name = "링 오브 고블린 로드[등급: 전설] ", ItemID = 100, Price = 10000, ItemType = 1, Inform = "고블린 로드가 장착했던 반지, ", //작업중
         IsEquiped = false, ShopFlag = false, MonsterFlag = true, Atk = 3f},
 
     };
