8f670741d6679fb5253dc8bec45c09e2d107931d
lolaop5047 <kthh9828@naver.com>
2025-02-11 12:44:17 +0900

    [GameManager] 아이템 수정 및 추가

--- GameManager.cs
+++ GameManager.cs
@@ -9,29 +9,29 @@ public class GameManager
 
     public List<Item> itemList = new List<Item>()
     {
-         new Weapon{Name = "투박한 롱소드", ItemID = 1, Price = 500, ItemType = 0, Inform = "매우 흔하디흔한 양산형 롱소드이다.",
+         new Weapon{Name = "투박한 롱소드", ItemID = 1, Price = 500, ItemType = 0, Inform = "매우 흔하디흔한 양산형, 롱소드이다.",
         IsEquiped = false, ShopFlag = true, MonsterFlag = false, Atk = 2f},
 
-        new Weapon{Name = "바스타드 소드", ItemID = 2, Price = 800, ItemType = 0, Inform = "대검 반 한 손 검, 롱소드보다 바디의 길이가 더 길며 파괴력이 높고 공방 밸런스가 잘 갖춰졌다.",
+        new Weapon{Name = "바스타드 소드", ItemID = 2, Price = 1150, ItemType = 0, Inform = "대검 반 한 손 검, 롱소드보다 바디의 길이가 더 길며 파괴력이 높고 공방 밸런스가 잘 갖춰졌다.",
         IsEquiped = false, ShopFlag = true, MonsterFlag = true, Atk = 4f},
 
         new Weapon{Name = "한손검 글라디우스", ItemID = 3, Price = 600, ItemType = 0, Inform = "폭 넓은 양날 장검 무거워서 신속하게 사용 힘듬, 방패랑 사용하면 안정적이다.",
+        IsEquiped = false, ShopFlag = true, MonsterFlag = true, Atk = 3f,},
+
+        new Armor{Name = "레더 아머", ItemID = 4, Price = 500, ItemType = 1, Inform = "가죽을 무질과 간단한 기름칠로 경화처리를 거친, 흔한 가죽 갑옷.",
+        IsEquiped = false, ShopFlag = true, MonsterFlag = false, Def = 1f},
+
+        new Armor{Name = "보일드 레더 아머", ItemID = 5, Price = 1000, ItemType = 1, Inform = "끓여진 파라핀+기름에 뺏다 넣었다 반복해서 만든 가죽 갑옷, 끝판왕 강도는 손으로 안굽 부러질 정도다.",
+        IsEquiped = false, ShopFlag = true, MonsterFlag = true, Def = 3f},
+
+        new Armor{Name = "고블린 찰갑", ItemID = 6, Price = 1600, ItemType = 1, Inform = "고블린 뼈로 만든 미늘 조각에 끈으로 연결하여 만든 갑옷, 꽤나 튼튼하고 유지 보수가 쉽다.",
+        IsEquiped = false, ShopFlag = true, MonsterFlag = true, Def = 5f},
+
+        new Consumable{Name = "회복 물약", ItemID = 7, Price = 50, ItemType = 2, Inform = "체력을 50 회복시켜 줍니다.", ShopFlag = true, MonsterFlag = false, BuffAmount = 50},
+
+        new Weapon{Name = "전설", ItemID = 100, Price = 10000, ItemType = 0, Inform = "전설",//작업중
         IsEquiped = false, ShopFlag = false, MonsterFlag = true, Atk = 3f},
 
-        new Weapon{Name = "전설", ItemID = 100, Price = 10000, ItemType = 0, Inform = "전설",
-        IsEquiped = false, ShopFlag = false, MonsterFlag = true, Atk = 3f},
-
-        new Armor{Name = "무쇠 갑옷", ItemID = 4, Price = 100, ItemType = 1, Inform = "흔히 구할 수 있는 갑옷입니다.",
-        IsEquiped = false, ShopFlag = true, MonsterFlag = false, Def = 5f},
-
-        new Armor{Name = "단단한 갑옷", ItemID = 5, Price = 300, ItemType = 1, Inform = "아주 단단한 갑옷입니다.",
-        IsEquiped = false, ShopFlag = true, MonsterFlag = true, Def = 8f},
-
-        new Armor{Name = "심상치 않은 갑옷", ItemID = 6, Price = 700, ItemType = 1, Inform = "심상치 않은 기운이 느껴지는 갑옷입니다.",
-        IsEquiped = false, ShopFlag = true, MonsterFlag = true, Def = 12f},
-
-
-        new Consumable{Name = "회복 물약", ItemID = 7, Price = 50, ItemType = 2, Inform = "체력을 50 회복시켜 줍니다.", ShopFlag = true, MonsterFlag = false, BuffAmount = 50}
     };
     public List<Equipment> equipments = new List<Equipment>();
     public List<Consumable> consumables = new List<Consumable>();
