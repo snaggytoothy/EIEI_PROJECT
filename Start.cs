5e261e314f1a31ffd11f68115af2f5ee69be3768
snaggytoothy <snaggytoothy@gmail.com>
2025-02-12 09:36:48 +0900

    [fix] 이름 길이 제한
    이름 설정할 때 글자 수를 8개로 제한하도록 함

--- Start.cs
+++ Start.cs
@@ -3,34 +3,43 @@ namespace EIEIE_Project;
 public class Start()
 {
     private static int restNum = 500; //휴식에 필요한 골드 양
-
+    public static string name = null;
     static void Main(string[] args)
     {
         GameManager gameManager = new(); //게임 매니저 생성
         StartGame(gameManager); //게임 시작
     }
 
+    public static void NameLength(GameManager gameManager) //이름의 길이를 8자로 제한함
+    {
+        while (true)
+        {
+            Console.WriteLine("스파르타 던전에 오신 것을 환영합니다.\r\n");
+            Console.WriteLine("원하시는 이름을 설정해주세요.");
+
+            name = Console.ReadLine();
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "스파르타"; //값이 없거나 빈 문자열이면 "스파르타"로 설정
+            }
+            char[] letters = name.ToCharArray();
+            if (letters.Length > 8) Console.WriteLine("8글자 내로 입력해주세요.");
+            else
+            {
+                gameManager.player.Name = name; //플레이어 이름 설정
+                break;
+            }
+        }
+    }
+
     public static void StartGame(GameManager gameManager)
     {
         while (true)
         {
-            while (true)
-            {
-                Console.WriteLine("스파르타 던전에 오신 것을 환영합니다.\r\n");
-                Console.WriteLine("원하시는 이름을 설정해주세요.");
+            NameLength(gameManager);
 
-                string str = Console.ReadLine() ?? "스파르타"; //값이 없다면 "스파르타"로 설정
-                char[] letters = str.ToCharArray();
-                if (letters.Length > 8) Console.WriteLine("8글자 내로 입력해주세요.");
-                else
-                {
-                    gameManager.player.Name = str; //플레이어 이름 설정
-                    break;
-                }
-            }
-            
             Console.Clear();
-            Console.WriteLine($"안녕하세요! {gameManager.player.Name}님!\n계속해서 이 이름으로 불러 드릴까요?");
+            Console.WriteLine($"안녕하세요! {name}님!\n계속해서 이 이름으로 불러 드릴까요?");
             Console.WriteLine("1. 네 \n2. 아니오");
 
             int namesure = Utility.GetInput(1, 2);
